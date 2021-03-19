aeb1e1a0249cf6f540564be8a75dc47dfee9b9f8
Alban Mouton <alban.mouton@gmail.com>
2021-03-19 15:11:47 +0100

    chore: syntax

--- extract.js
+++ extract.js
@@ -3,20 +3,17 @@ const semver = require('semver')
 module.exports = (rawVersion, params) => {
   const v = semver.coerce(rawVersion)
   if (!v) {
-    if (params.branch) {
-      return rawVersion
-      return
-    }
+    if (params.branch) return rawVersion
     throw new Error(`The input version ${rawVersion} cannot be coerced to semver`)
   }
 
   if (params.major) {
     if (params.X) return `${v.major}.x`
-    else return `${v.major}`
-  } else if (params.minor) {
-    if (params.X) return `${v.major}.${v.minor}.x`
-    else return `${v.major}.${v.minor}`
-  } else {
-    return v.version
+    return `${v.major}`
   }
+  if (params.minor) {
+    if (params.X) return `${v.major}.${v.minor}.x`
+    return `${v.major}.${v.minor}`
+  }
+  return v.version
 }
